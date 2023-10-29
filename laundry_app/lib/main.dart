a8278eb7e4318b048456ea9add8ffc4a08a334e0
Kevin Ying <ying.kev@gmail.com>
2023-10-29 14:37:35 +1100

    progress

--- laundry_app/lib/main.dart
+++ laundry_app/lib/main.dart
@@ -1,125 +1,221 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'dart:ffi';
 
-void main() {
-  runApp(const MyApp());
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class ForecastEvent {
+  final double cloudScore;
+  final double humidityScore;
+  final double isgoodScore;
+
+  const ForecastEvent({
+    required this.cloudScore,
+    required this.humidityScore,
+    required this.isgoodScore,
+  });
+
+  factory ForecastEvent.fromJson(Map<String, dynamic> json) {
+    return ForecastEvent(
+        cloudScore: json['main']['humidity'] * 1.0,
+        humidityScore: json['clouds']['all'] * 1.0,
+        isgoodScore: 0);
+  }
 }
 
-class MyApp extends StatelessWidget {
+class ForecastResponse {
+  // final int userId;
+  // final int id;
+  // final String title;
+  final int cnt;
+  final List<ForecastEvent> events;
+
+  const ForecastResponse({
+    // required this.userId,
+    // required this.id,
+    // required this.title,
+    required this.cnt,
+    required this.events,
+  });
+
+  factory ForecastResponse.fromJson(Map<String, dynamic> json) {
+    var events = [for (var e in json['list']) ForecastEvent.fromJson(e)];
+
+    return ForecastResponse(
+      // userId: json['userId'] as int,
+      // id: json['id'] as int,
+      // title: json['title'] as String,
+      cnt: json['cnt'] as int,
+      events: events,
+    );
+  }
+}
+
+Future<ForecastResponse> fetchForecastResponse() async {
+  final response = await http.get(Uri.parse(
+      'https://api.openweathermap.org/data/2.5/forecast?lat=-34.0192056&lon=151.0658292&appid=4fbe2c3c02d76185e9ebb7e2c24b54d0&units=metric'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return ForecastResponse.fromJson(
+        jsonDecode(response.body) as Map<String, dynamic>);
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load Forecast response');
+  }
+}
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late Future<ForecastResponse> futureForecastResponse;
+
+  @override
+  void initState() {
+    super.initState();
+    futureForecastResponse = fetchForecastResponse();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Fetch Data Example',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Fetch Data Example'),
+        ),
+        body: Center(
+          child: FutureBuilder<ForecastResponse>(
+            future: futureForecastResponse,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                // return Text(snapshot.data!.cnt.toString());
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+                // parse the response
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+                return ListView.builder(
+                  itemCount: snapshot.data!.cnt,
+                  itemBuilder: (context, index) {
+                    final item = HeadingItem(
+                        'CloudScore ${snapshot.data!.events[index].cloudScore.toString()}');
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+                    return ListTile(
+                      title: item.buildTitle(context),
+                      subtitle: item.buildSubtitle(context),
+                    );
+                  },
+                );
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+              // By default, show a loading spinner.
+              return const CircularProgressIndicator();
+            },
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+// void main() {
+//   runApp(
+//     MyApp(
+//       items: List<ListItem>.generate(
+//         1000,
+//         (i) => i % 6 == 0
+//             ? HeadingItem('Heading $i')
+//             : MessageItem('Sender $i', 'Message body $i'),
+//       ),
+//     ),
+//   );
+// }
+
+// class MyApp extends StatelessWidget {
+//   final List<ListItem> items;
+
+//   const MyApp({super.key, required this.items});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     const title = 'Laundry Days';
+
+//     return MaterialApp(
+//       title: title,
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text(title),
+//         ),
+//         body: ListView.builder(
+//           // Let the ListView know how many items it needs to build.
+//           itemCount: items.length,
+//           // Provide a builder function. This is where the magic happens.
+//           // Convert each item into a widget based on the type of item it is.
+//           itemBuilder: (context, index) {
+//             final item = items[index];
+
+//             return ListTile(
+//               title: item.buildTitle(context),
+//               subtitle: item.buildSubtitle(context),
+//             );
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+/// The base class for the different types of items the list can contain.
+abstract class ListItem {
+  /// The title line to show in a list item.
+  Widget buildTitle(BuildContext context);
+
+  /// The subtitle line, if any, to show in a list item.
+  Widget buildSubtitle(BuildContext context);
+}
+
+/// A ListItem that contains data to display a heading.
+class HeadingItem implements ListItem {
+  final String heading;
+
+  HeadingItem(this.heading);
+
+  @override
+  Widget buildTitle(BuildContext context) {
+    return Text(
+      heading,
+      style: Theme.of(context).textTheme.headlineSmall,
+    );
+  }
+
+  @override
+  Widget buildSubtitle(BuildContext context) => const SizedBox.shrink();
+}
+
+/// A ListItem that contains data to display a message.
+class MessageItem implements ListItem {
+  final String sender;
+  final String body;
+
+  MessageItem(this.sender, this.body);
+
+  @override
+  Widget buildTitle(BuildContext context) => Text(sender);
+
+  @override
+  Widget buildSubtitle(BuildContext context) => Text(body);
 }
